12f458fe16c536f03ba6b6f870036756698f117f
Bryan Xu <bryanbobo123@gmail.com>
2021-04-14 14:37:56 -0700

    bx - removed cors package

--- api.py
+++ api.py
@@ -11,8 +11,6 @@ def get_current_time():
 def index():
   return "<h1>Welcome to our server!</h1>"
 
-app.use(cors());
-
 if __name__ == '__main__':
   # Threaded option to enable multiple instances for multiple user access support
   app.run(threaded=True, port=5000, debug=True)
